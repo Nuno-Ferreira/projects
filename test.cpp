a0b13847994014665910b69bed1f728e52542e06
Nuno Ferreira <nunomiguelferreira5@gmail.com>
2023-02-18 22:00:38 +0000

    first tries in c++

---
+++ test.cpp
@@ -0,0 +1,24 @@
+// this is a comment 
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cout << "Please enter your name: ";
+    cin >> str;
+    cout << "Hello, " << str << "!" << endl;
+    return 0;
+}
+
+
+// LOGIN REGISTRATION PROJECT
+// so need to think about how to really work with the data that I input and how to store it effectively 
+// asks username and password
+// creates user file with credentials
+// if user does not exist upon login, throw error
+
+// STUDENT GRADING SYSTEM
+// use this to maybe calculate my grades and keep track of progress
