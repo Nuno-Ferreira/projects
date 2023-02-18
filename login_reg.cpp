842ec8fd3967770e9473aea80bb17d080860c129
Nuno Ferreira <nunomiguelferreira5@gmail.com>
2023-02-18 22:01:07 +0000

    started a login and reg project

---
+++ login_reg.cpp
@@ -0,0 +1,16 @@
+// LOGIN REGISTRATION PROJECT
+// so need to think about how to really work with the data that I input and how to store it effectively 
+// asks username and password
+// creates user file with credentials
+// if user does not exist upon login, throw error
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string user_name;
+    string password;
+    
+}
