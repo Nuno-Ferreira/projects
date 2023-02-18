14bee4b375a1e079ef9e5bb51b38f001bb2d9ffd
Nuno Ferreira <nunomiguelferreira5@gmail.com>
2023-02-18 22:41:02 +0000

    added some comments and the credentials file opening

--- login_reg.cpp
+++ login_reg.cpp
@@ -17,16 +17,27 @@ int main(){
     string password;
 
     // create login credentials file
-    FILE* fp = fopen("~/Documents/projects/login_credentials.txt","w+");
+    FILE* file = fopen("~/Documents/projects/login_credentials.txt","r+");
+
+    // check if file exists or not
+    if (file!=NULL) {
+        file = fopen("~/Documents/projects/login_credentials.txt","r+");
+    }
+    else {
+        file = fopen("~/Documents/projects/login_credentials.txt","w+");
+    }
 
 
+    // terminal interface for login
     cout << "Hello! Please login with your credentials";
     cout << "Username: ";
     cin >> user_name;
     cout << "Password: ";
     cin >> password;
+
+
     // need to add an if statement to check if the user is registered
     // need to have a second part to do the registration
     // need to add a part to create the file to add the login credentials to
-
+    return 0;
 }
