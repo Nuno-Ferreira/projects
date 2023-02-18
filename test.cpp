3e90688ec633910076ac946ad659eacc86a716be
Nuno Ferreira <nunomiguelferreira5@gmail.com>
2023-02-18 22:30:33 +0000

    deleted some comments

--- test.cpp
+++ test.cpp
@@ -14,11 +14,5 @@ int main(){
 }
 
 
-// LOGIN REGISTRATION PROJECT
-// so need to think about how to really work with the data that I input and how to store it effectively 
-// asks username and password
-// creates user file with credentials
-// if user does not exist upon login, throw error
-
 // STUDENT GRADING SYSTEM
 // use this to maybe calculate my grades and keep track of progress
